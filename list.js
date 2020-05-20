c215a471a0f39536d4436e4ae5addb676ccbb16d
Jimmy Echternach <jimmynechternach@gmail.com>
2020-05-20 11:56:08 -0700

    finished constructor

--- list.js
+++ list.js
@@ -2,13 +2,27 @@ class List {
     constructor() {
         this.items = []
         this.addItems = (item) => {
-            this.items.push(item)
+            this.items.push({ name: item, isDone: false })
         }
-    
+        this.getItems = () => {
+            return this.items
+        }
+        this.updateItem = (name) => {
+            this.items.forEach((item) => {
+                if (item.name === name) {
+                    item.isDone = !item.isDone
+                }
+            })
+        }
+        this.deleteItem = (name) => {
+            this.items = this.items.filter((item) => {
+                if (item.name === name) {
+                    return false
+                } else {
+                    return true
+                }
+            })
+        }
     }
-
-
-
-
 }
 module.exports = List
